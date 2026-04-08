5e24d1371abfb1f81a51f975cc4aaa8795eb5de2
uanela <uanelaluiswayne@gmail.com>
2026-04-08 08:57:41 +0200

    chore(plugins): disable plugin challenge configuration

--- lua/plugins/nvim-plugin-challenges.lua
+++ lua/plugins/nvim-plugin-challenges.lua
@@ -3,10 +3,10 @@ local name = "nvim-project-root"
 local plugin_name = name:gsub("-challenge", "")
 
 return {
-  plugin_name,
-  dir = "~/Documents/development/lua/solutions-nvim-plugin-challgens/" .. number .. "-" .. name,
-  config = function()
-    -- print("loading... " .. plugin_name)
-    -- require(plugin_name).setup()
-  end
+  -- plugin_name,
+  -- dir = "~/Documents/development/lua/solutions-nvim-plugin-challgens/" .. number .. "-" .. name,
+  -- config = function()
+  --   -- print("loading... " .. plugin_name)
+  --   require(plugin_name).setup()
+  -- end
 }
